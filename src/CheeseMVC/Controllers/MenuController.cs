96cfb3c20607ac3e600ec4751e13cbd4da31a262
Timothy Landwehr <tjlandwehr@gmail.com>
2018-03-14 02:15:18 -0500

    Added Menu options showing many-to-many relationship with cheeses

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Menu> menus = context.Menus.ToList();
+            return View(menus);
+        }
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuviewModel = new AddMenuViewModel();
+            return View(addMenuviewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new menu to my existing menus
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+
+        public IActionResult ViewMenu(int id)
+        {
+            List<CheeseMenu> items = context
+                .CheeseMenus
+                .Include(item => item.Cheese)
+                .Where(cm => cm.MenuID == id)
+                .ToList();
+
+            Menu menu = context.Menus.Single(m => m.ID == id);
+
+            ViewMenuViewModel viewModel = new ViewMenuViewModel
+            {
+                Menu = menu,
+                Items = items
+            };
+
+            return View(viewModel);
+        }
+
+        // /Menu/AddItem/3
+        public IActionResult AddItem(int id)
+        {
+            Menu menu = context.Menus.Single(m => m.ID == id);
+            List<Cheese> cheeses = context.Cheeses.ToList();
+            return View(new AddMenuItemViewModel(menu, cheeses));
+        }
+
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cheeseID = addMenuItemViewModel.CheeseID;
+                var menuID = addMenuItemViewModel.MenuID;
+
+                IList<CheeseMenu> existingItems = context.CheeseMenus
+                    .Where(cm => cm.CheeseID == cheeseID)
+                    .Where(cm => cm.MenuID == menuID).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        Cheese = context.Cheeses.Single(c => c.ID == cheeseID),
+                        Menu = context.Menus.Single(m => m.ID == menuID)
+                    };
+
+                    context.CheeseMenus.Add(menuItem);
+                    context.SaveChanges();
+                }
+
+                return Redirect(string.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel.MenuID));
+            }
+
+            return View(addMenuItemViewModel);
+        }
+    }
+}
--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,4 +1,6 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
@@ -8,5 +10,7 @@
 
         public int CategoryID { get; set; }
         public CheeseCategory Category { get; set; }
+
+        public List<CheeseMenu> CheeseMenus { get; set; }
     }
 }
